4748fa9d81be70cc69df00ade6fe2e36c3e6d371
Douglas Queiroz <queirozdouglas466@gmail.com>
2024-11-13 14:32:17 -0300

    Items management

--- routes/pedidoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createPedido } = require("../controllers/pedidoController");
-
-const router = express.Router();
-
-router.post("/", createPedido);
-
-module.exports = router;
