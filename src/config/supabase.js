22df1669946cdd597bb190e2050bd97f37e427f0
Douglas Queiroz <queirozdouglas466@gmail.com>
2024-11-15 10:02:52 -0300

    adjusted config

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,5 +1,6 @@
-config.require("dotenv");
+import { config } from "dotenv";
 const { createClient } = "@supabase/supabase-js";
+config("dotenv");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
