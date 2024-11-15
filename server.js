89cbbee14693432fd80f0d5b10a15b5e79c973be
Douglas Queiroz <queirozdouglas466@gmail.com>
2024-11-15 16:50:17 -0300

    add ports to run

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const enterpriseRoutes = require("./src/routes/enterpriseRoutes");
 const itemRoutes = require("./src/routes/itemRoutes");
