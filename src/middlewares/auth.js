9b125a597a4f15c015f6ed4cbc86122a6cd214ca
Douglas Queiroz <queirozdouglas466@gmail.com>
2024-11-16 08:41:07 -0300

    +1 test

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,14 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 const authenticateToken = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "No token provided" });
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) return res.status(401).json({ error: "Token não fornecido" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ error: "Invalid token" });
+    if (err) return res.status(403).json({ error: "Token inválido" });
     req.user = user;
     next();
   });
 };
 
-module.exports = authenticateToken;
+module.exports = { authenticateToken };
